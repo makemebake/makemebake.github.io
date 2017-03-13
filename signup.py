e9a0256154bf6931c61b179aa920c3109a65dba8
sophierajux <sophieraju@hotmail.co.uk>
2017-03-13 19:06:03 +0000

    Create signup.py

---
+++ signup.py
@@ -0,0 +1,22 @@
+from flask import Flask 
+from flask import render_template 
+from flask import request 
+
+app = Flask ("MyApp")
+
+
+@app.route("/")
+def signup():
+	return render_template("enter.html")
+
+@app.route("/signup", methods=['POST'])
+def sign_up():
+	form_data = request.form
+	print form_data['name']
+	print form_data['email']
+	
+	return render_template("finished.html")
+
+
+
+app.run()
